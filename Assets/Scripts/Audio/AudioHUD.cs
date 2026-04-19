87873201d0e4f8f49667e05a4755ba70a8b4006e
clarasmith-sound <clarasmith.sound@proton.me>
2026-04-19 13:30:23 -0700

    Updated Audio Banks
    Made a lot of tuning adjustments to the FMOD project for better mix, music control, wind tuning, enemy stationary audio. will be tuning attenuations in game next. Updated prefabs with music cue fabs so i dont have to keep putting them back in

--- Assets/Scripts/Audio/AudioHUD.cs
+++ Assets/Scripts/Audio/AudioHUD.cs
@@ -3,12 +3,50 @@ using FMODUnity;
 
 public class AudioHUD : MonoBehaviour
 {
+    public EventReference TutorialDingEvent;
     public EventReference DangerAlertEvent;
     public EventReference LowFoodAlertEvent;
     public EventReference AchievementAlertEvent;
 
+    public void PlayTutorialDingAlert() => MusicManager.PlayOneShot(TutorialDingEvent);
     public void PlayDangerAlert() => MusicManager.PlayOneShot(DangerAlertEvent);
     public void PlayLowFoodAlert() => MusicManager.PlayOneShot(LowFoodAlertEvent);
     public void PlayAchievementAlert() => MusicManager.PlayOneShot(AchievementAlertEvent);
+
+    #region Special 1 Time Stingers
+    public EventReference FirstAttackNotify;
+    private bool bDidFirstAttackNotifyPlay = false;
+
+    public EventReference FirstAntSpawnNotify;
+    private bool bDidFirstAntSpawnNotifyPlay = false;
+
+    public EventReference FirstCarpenterAntBuilding;
+    private bool bDidFirstCarpenterAntBuildingPlay = false;
+
+    public void PlayFirstAttackNotify()
+    {
+        if(bDidFirstAttackNotifyPlay == false)
+        {
+            bDidFirstAttackNotifyPlay = true;
+            MusicManager.PlayOneShot(FirstAttackNotify);
+        }
+    }
+    public void PlayFirstAntSpawnNotify()
+    {
+        if(bDidFirstAntSpawnNotifyPlay == false)
+        {
+            bDidFirstAntSpawnNotifyPlay = true;
+            MusicManager.PlayOneShot(FirstAntSpawnNotify);
+        }
+    }
+    public void PlayFirstCarpenterAntBuilding()
+    {
+        if(bDidFirstCarpenterAntBuildingPlay == false)
+        {
+            bDidFirstCarpenterAntBuildingPlay = true;
+            MusicManager.PlayOneShot(FirstCarpenterAntBuilding);
+        }
+    }
+    #endregion
 
 }
