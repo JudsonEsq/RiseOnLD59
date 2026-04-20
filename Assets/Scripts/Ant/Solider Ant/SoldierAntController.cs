f4c86574f8b4e4cdac2b5b4c8bfe43f0fd17575a
Matt Monroe <matt.a.monroe@gmail.com>
2026-04-20 11:36:17 -0600

    Add food carry object to all ants

--- Assets/Scripts/Ant/Solider Ant/SoldierAntController.cs
+++ Assets/Scripts/Ant/Solider Ant/SoldierAntController.cs
@@ -5,6 +5,7 @@ public class SoldierAntController : AntController
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        Init();
         antType = AntType.Soldier;
         targetPheromones.Add(Pheromone.PheromoneType.Danger);
         targetPheromones.Add(Pheromone.PheromoneType.Food);
