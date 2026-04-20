842bc2335c3879dd1eea49584e699f70d5c04b6c
Jude Holt <jude@holt-info.com>
2026-04-20 14:08:03 -0400

    Implemented title menu wiggle
    Added fields to control parameters of title wiggle curve.

--- Assets/Scripts/TitleMenuAnimation.cs
+++ Assets/Scripts/TitleMenuAnimation.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class TitleMenuAnimation : MonoBehaviour
 {
     public GameObject[] titleLetters;
+    private float[] originalPos;
     public float timer;
     public int interval = 0;
+
+    [SerializeField, Tooltip("How high the letters go up")] private float amplitude = 1f;
+    [SerializeField, Tooltip("How often the letters move")] private float frequency = 1f;
+    [SerializeField, Tooltip("Lowest value the position should clamp")] private float floor = 0.3f;
+    [SerializeField, Tooltip("Highest value the position should clamp")] private float ceil = 3f;
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         timer = 0f;
+
+        originalPos = new float[titleLetters.Length];
+        for(int i = 0; i < titleLetters.Length; i++)
+        {
+            originalPos[i] = titleLetters[i].transform.position.y;
+        }    
     }
 
     // Update is called once per frame
@@ -24,7 +37,9 @@ public class TitleMenuAnimation : MonoBehaviour
             for(int i = 0; i < titleLetters.Length; i++)
             {               
                 Transform tempLetter = titleLetters[i].transform;
-                tempLetter.position = new Vector3(tempLetter.position.x, Mathf.Clamp(3 * MathF.Sin((timer - i) / 3.5f), 0, 3), tempLetter.position.z);
+                Vector3 oldPos = tempLetter.position;
+                oldPos.y = originalPos[i] + Mathf.Clamp(MathF.Sin((timer - i) * frequency) * amplitude, floor, ceil);
+                tempLetter.position = oldPos;
             }
         }
     }
