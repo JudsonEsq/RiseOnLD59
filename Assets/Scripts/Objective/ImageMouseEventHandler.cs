5a15014ddb397305f9bcf7099769baa5a8d0b488
briggsbu <justin.hilburn@gmail.com>
2026-04-20 11:45:24 -0400

    Add Serialized Fields to Objective Panel

--- Assets/Scripts/Objective/ImageMouseEventHandler.cs
+++ Assets/Scripts/Objective/ImageMouseEventHandler.cs
@@ -11,7 +11,16 @@ public class ImageMouseEventHandler : MonoBehaviour, IPointerEnterHandler, IPoin
 
     bool MouseInObjectivePanel = false;
 
-    DateTime MouseExitedObjectivePanel;
+    DateTime? MouseExitedObjectivePanel;
+
+    [SerializeField]
+    int FadeOutTimeSeconds = 10;
+
+    [SerializeField]
+    int LoadDelayBeforeInitialFadeSeconds = 20;
+
+    [SerializeField]
+    float MinimumOpacity = .2f;
 
     public void OnPointerEnter(PointerEventData eventData)
     {
@@ -19,8 +28,7 @@ public class ImageMouseEventHandler : MonoBehaviour, IPointerEnterHandler, IPoin
 
         MouseInObjectivePanel = true;
 
-        ObjectiveDisplay.color = new Color(ObjectiveDisplay.color.r, ObjectiveDisplay.color.g, ObjectiveDisplay.color.b, 1.0f);
-        ObjectiveBackground.color = new Color(ObjectiveBackground.color.r, ObjectiveBackground.color.g, ObjectiveBackground.color.b, 1.0f);
+        SetOpacity(1.0f);
     }
 
     public void OnPointerExit(PointerEventData eventData)
@@ -31,27 +39,35 @@ public class ImageMouseEventHandler : MonoBehaviour, IPointerEnterHandler, IPoin
         MouseExitedObjectivePanel = DateTime.Now;
     }
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         ObjectiveDisplay = GetComponentInChildren<TextMeshProUGUI>();
         ObjectiveBackground = GetComponentInChildren<Image>();
 
-        Debug.Log("ObjectiveBackground: " + ObjectiveBackground == null ? "Null" : "Not Null");
+        Debug.Log("ObjectiveBackground: " + (ObjectiveBackground == null ? "Null" : "Not Null"));
+
+        SetOpacity(1.0f);
+
+        MouseExitedObjectivePanel = DateTime.Now + TimeSpan.FromSeconds(LoadDelayBeforeInitialFadeSeconds);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(!MouseInObjectivePanel)
+        if(!MouseInObjectivePanel && MouseExitedObjectivePanel.HasValue)
         {
-            Color ObjectiveColor = ObjectiveDisplay.color;
-            Color BackgroundColor = ObjectiveBackground.color;
+            float Opacity = Math.Max(MinimumOpacity, 1 - (float)(DateTime.Now - MouseExitedObjectivePanel.Value).TotalSeconds / FadeOutTimeSeconds);
 
-            float Opacity = Math.Max(.2f, 1 - (float)(DateTime.Now - MouseExitedObjectivePanel).TotalSeconds / 10);
-
-            ObjectiveDisplay.color = new Color(ObjectiveColor.r, ObjectiveColor.g, ObjectiveColor.b, Opacity);
-            ObjectiveBackground.color = new Color(BackgroundColor.r, BackgroundColor.g, BackgroundColor.b, Opacity);
+            SetOpacity(Opacity);
         }
+    }
+
+    internal void SetOpacity(float opacity)
+    {
+        Color ObjectiveColor = ObjectiveDisplay.color;
+        Color BackgroundColor = ObjectiveBackground.color;
+
+        ObjectiveDisplay.color = new Color(ObjectiveColor.r, ObjectiveColor.g, ObjectiveColor.b, opacity);
+        ObjectiveBackground.color = new Color(BackgroundColor.r, BackgroundColor.g, BackgroundColor.b, opacity);
     }
 }
